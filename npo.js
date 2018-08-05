9c4512aafa67cce575dc6ab03b5594a065ec0ca2
木村　幸介 <kosukekimura0528@gmail.com>
2018-08-06 00:04:34 +0900

    practice for scraping.first commit.

---
+++ npo.js
@@ -0,0 +1,52 @@
+const client = require("cheerio-httpcli");
+const fs = require("fs");
+const csv = require("csv");
+
+var url = "http://www.nta.go.jp/taxes/tetsuzuki/shinsei/npo/meibo/03.htm";
+var params = {};
+
+
+client.fetch(url,params,function(err,$,response){
+    //エラーだったらメッセージを出す
+    if(err){
+        console.log("err:"+err);
+        return;
+    };
+
+    //エラー以外の場合
+    const header = {
+        name:"法人名称",
+        number:"法人番号",
+        address:"主たる事務所の所在地",
+        rep:"代表者",
+        validity:"有効期限"
+    };
+
+    // console.log($);
+    // console.log($.xml());
+
+    var body =$.html();
+    // console.log(body);
+    // console.log(body.length);
+    // console.log(body[0]);
+    // console.log(body[100]);
+    // console.log(body[1000]);
+    // console.log(body[10000]);
+
+    const stringifier = csv.stringify({header:false,columns:header});
+    // const writeableStream = fs.createWriteStream('npo.csv',{encoding:'utf-8'});
+    // stringifier.pipe(writeableStream);
+
+    var accum_txt = "";
+    $("tr>td").each(function(idx){
+        var text = $(this).text();
+        stringifier.pipe(text);
+        if((idx+1)%5===0){
+            stringifier.write("\n");
+        }
+    });
+
+});
+
+
+
